e2c3cd70574073f4543e1b5b9d9d359286859d64
looechao <lu.chao.personal@foxmail.com>
2024-07-27 20:06:27 +0800

    update

--- blog.config.js
+++ blog.config.js
@@ -6,8 +6,8 @@ const BLOG = {
   description: '我的notion树洞.',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
-  appearance: 'dark', // ['light', 'dark', 'auto'],
-  font: 'serif', // ['sans-serif', 'serif']
+  appearance: 'auto', // ['light', 'dark', 'auto'],
+  font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
